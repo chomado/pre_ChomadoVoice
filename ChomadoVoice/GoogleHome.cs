ed71dc865e96b402896d8ac9b141f059d0f832ca
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2018-03-15 14:36:32 +0900

    Voice Text Web API 対応

--- ChomadoVoice/GoogleHome.cs
+++ ChomadoVoice/GoogleHome.cs
@@ -1,46 +1,57 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Newtonsoft.Json;
+using VoiceTextWebAPI.Client;
 
 namespace ChomadoVoice
 {
     public static class GoogleHome
     {
         [FunctionName("GoogleHome")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req
+            , /* Azure Blob Storage(ファイル置き場) への出力 */ [Blob("mp3/voice.mp3", FileAccess.ReadWrite)] CloudBlockBlob mp3Out
+            , TraceWriter log
+        )
         {
             log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            /*string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;*/
-
-            // Get request body
             var data = await req.Content.ReadAsAsync<Models.DialogFlowResponseModel>();
             //log.Info(data);
-            //var json = await req.Content.ReadAsStringAsync();
             var say = data.Result.ResolvedQuery;
+
+            var voiceTextClient = new VoiceTextClient
+            {
+                APIKey = Keys.APIKeys.VoiceTextWebApiKey,
+                Speaker = Speaker.Bear,
+                Emotion = Emotion.Anger,
+                EmotionLevel = EmotionLevel.High,
+                Format = Format.MP3
+            };
+            var bytes = await voiceTextClient.GetVoiceAsync(text: say);
+
+            // Azure Blob Storage への書き込み（保存）
+            await mp3Out.UploadFromByteArrayAsync(buffer: bytes, index: 0, count: bytes.Length);
+
+            // Azure Blob Storage に書き込まれた mp3 にアクセスするための URL
+            var mp3Url = mp3Out.Uri;
 
             var result = req.CreateResponse(HttpStatusCode.OK, new
             {
-                speech = $"あなたは{say}と言いましたね",
-                displayText = $"あなたは{say}と言いましたね"
+                // Google Home に喋らせたい文言を渡す。（この場合mp3）
+                speech = $"<speak><audio src='{mp3Url}' /></speak>",
+                // Google Assistant のチャット画面上に出したい文字列
+                displayText = $"あなたは{say}と言いましたね。"
             });
             result.Headers.Add("ContentType", "application/json");
             return result;
-
-            // Set name to query string or body data
-            //name = name ?? data?.name;
-
-            //return name == null
-            //    ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-            //    : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
+        
     }
 }
